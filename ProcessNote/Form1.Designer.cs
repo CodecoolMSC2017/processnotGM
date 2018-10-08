fd26c4f8397e86027fc92d6610e5bca092a84f80
mtganyu <33490825+mtganyu@users.noreply.github.com>
2018-10-08 20:50:45 +0200

    Init geco

---
+++ ProcessNote/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ProcessNote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.NameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.idColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.NameColumn,
+            this.idColumn});
+            this.listView1.Location = new System.Drawing.Point(270, 30);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(494, 230);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView1_ItemSelectionChanged);
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.Text = "Name";
+            this.NameColumn.Width = 422;
+            // 
+            // idColumn
+            // 
+            this.idColumn.Text = "Id";
+            this.idColumn.Width = 396;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(50, 90);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(50, 145);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(50, 403);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Always on top";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ColumnHeader NameColumn;
+        private System.Windows.Forms.ColumnHeader idColumn;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ ProcessNote/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessNote
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            Process process = new Process();
+            process.StartInfo.FileName = text;
+            process.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadProcessList();
+        }
+
+        private void loadProcessList()
+        {
+            listView1.Items.Clear();
+            Process[] processes = Process.GetProcesses();
+            foreach (Process p in processes)
+            {
+                string[] tag = new string[] { p.ProcessName, p.Id.ToString() };
+                ListViewItem item = new ListViewItem(tag);
+                item.Tag = p;
+                listView1.Items.Add(item);
+            }
+        }
+
+     
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = listView1.SelectedItems[0];
+            Process process = (Process)item.Tag;
+            process.Kill();
+            loadProcessList();
+            
+        }
+
+      
+
+        private void listView1_MouseHover(object sender, EventArgs e)
+        {
+            ListViewItem item = listView1.SelectedItems[0];
+            Process process = (Process)item.Tag;
+            MessageBox.Show("\nprocess  Name : " + process.ProcessName + "\nprocess Time : " + process.PrivilegedProcessorTime.ToString() + "\nprocess Threads : " + process.Threads.Count + "\nprocess StartTime : " + process.StartTime + "\nprocess Memory Size : " + process.PrivateMemorySize);
+
+        }
+
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            if (e.IsSelected)
+            { 
+
+            ListViewItem item = e.Item;
+            
+            Process process = (Process)item.Tag;
+            MessageBox.Show("\nprocess  Name : " + process.ProcessName + "\nprocess Time : " + process.PrivilegedProcessorTime.ToString() + "\nprocess Threads : " + process.Threads.Count + "\nprocess StartTime : " + process.StartTime + "\nprocess Memory Size : " + process.PrivateMemorySize);
+               
+            }
+        }
+        
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(TopMost == false) {
+            TopMost = true;
+            }
+            else
+            {
+                TopMost = false;
+            }
+        }
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
+
